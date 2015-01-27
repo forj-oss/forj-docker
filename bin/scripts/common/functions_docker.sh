25c1dfecf169b1f7df15e3267235a2df544ba496
Edward Raigosa (wenlock) <wenlock@hp.com>
2015-01-27 13:18:31 -0800

    restart delay and unset proxy
    We need to unset the proxy when http_proxy is not set.
    And restarting the service should have a check for it
    to be up and running.
    
    Change-Id: I486ecbf1e6de53e567e08593883db67ad761c292

--- bin/scripts/common/functions_docker.sh
+++ bin/scripts/common/functions_docker.sh
@@ -26,23 +26,30 @@ function DOCKER_DEFAULTS {
 #
 # restart docker services
 function DOCKER_RESTART {
-  echo 'Restarting docker service'
-  DO_SUDO service docker restart
-  return 0
+    echo 'Restarting docker service'
+    DO_SUDO service docker restart
+    timeout=0
+    until docker images > /dev/null 2<&1 || [ $timeout -eq 4 ]; do
+        sleep $(( timeout++ ))
+    done
+    docker images > /dev/null 2<&1 ||
+    (echo 'failed to start docker service' && return 1)
+    echo 'docker service restarted'
+    return 0
 }
 
 #
 # DOCKER_GRANT_ACCESS 'username'
 # grant access to current user or passed in user
 function DOCKER_GRANT_ACCESS {
-  if [ -z "${1}" ] ; then
-    CURRENT_USER=$(facter id)
-  else
-    CURRENT_USER=$1
-  fi
-  [ -z "${CURRENT_USER}" ] && ERROR_EXIT ${LINENO} "failed to get current user with facter id" 2
-  DO_SUDO puppet apply $PUPPET_DEBUG \
-  -e "user {'${CURRENT_USER}': ensure => present, gid => 'docker' }"
+    if [ -z "${1}" ] ; then
+        CURRENT_USER=$(facter id)
+    else
+        CURRENT_USER=$1
+    fi
+    [ -z "${CURRENT_USER}" ] && ERROR_EXIT ${LINENO} "failed to get current user with facter id" 2
+    DO_SUDO puppet apply $PUPPET_DEBUG \
+    -e "user {'${CURRENT_USER}': ensure => present, gid => 'docker' }"
 }
 
 #
@@ -51,9 +58,14 @@ function DOCKER_PROXY_CONF {
   # check if the local docker config has /etc/default/docker configured
   # for http_proxy
   if [ ! -z "$HTTP_PROXY" ]; then
-    egrep ".*export\shttp_proxy=\"${HTTP_PROXY}\"" "$(DOCKER_DEFAULTS)" ||
-    DO_SUDO sed -n -i '/.*export\shttp_proxy=.*/!p' "$(DOCKER_DEFAULTS)" &&
-    DO_SUDO bash -c "echo \"export http_proxy=\"${HTTP_PROXY}\"\" >> \"$(DOCKER_DEFAULTS)\"" &&
-    DOCKER_RESTART
+      egrep ".*export\shttp_proxy=\"${HTTP_PROXY}\"" "$(DOCKER_DEFAULTS)" ||
+      (DO_SUDO sed -n -i '/.*export\shttp_proxy=.*/!p' "$(DOCKER_DEFAULTS)" &&
+      DO_SUDO bash -c "echo \"export http_proxy=\\\"${HTTP_PROXY}\\\"\" >> \"$(DOCKER_DEFAULTS)\"" &&
+      DOCKER_RESTART)
+  else
+      egrep ".*export\shttp_proxy=.*" "$(DOCKER_DEFAULTS)" &&
+      (DO_SUDO sed -n -i '/.*export\shttp_proxy=.*/!p' "$(DOCKER_DEFAULTS)" &&
+      echo 'removing proxy conf from docker' &&
+      DOCKER_RESTART)
   fi
 }
